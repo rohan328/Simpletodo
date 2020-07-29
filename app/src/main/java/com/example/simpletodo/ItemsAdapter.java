4b326466de242d7ae24e28041549f6f83b3547ca
Rohan <35950830+rohan328@users.noreply.github.com>
2020-07-29 14:08:41 +0530

    add and delete feature added

--- app/src/main/java/com/example/simpletodo/ItemsAdapter.java
+++ app/src/main/java/com/example/simpletodo/ItemsAdapter.java
@@ -1,4 +1,79 @@
 package com.example.simpletodo;
 
-public class ItemsAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+//responsible for displaying data onto the rows of recycler view
+public class ItemsAdapter extends RecyclerView.Adapter<ItemsAdapter.ViewHolder> {
+
+    public interface OnLongClickListener{
+        void onItemLongClicked(int position);
+
+    }
+
+    List<String> items;
+    OnLongClickListener longClickListener;
+
+    public ItemsAdapter(List<String> items,OnLongClickListener longClickListener) {
+        this.items = items;
+        this.longClickListener=longClickListener;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        //Use a layout inflator to inflate a view
+
+        View todoview = LayoutInflater.from(parent.getContext()).inflate(android.R.layout.simple_list_item_1, parent, false);
+
+        //wrap it inside a viewholder and return
+        return new ViewHolder(todoview);
+    }
+
+    //resposible for binding data to a particular view holder
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        //grab the item at position
+        String item = items.get(position);
+        //bind the item into specified view holder
+        holder.bind(item);
+    }
+
+    //tells the RV how many items are in the list
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+
+    //container to provide easy access to views that represent each row of the list
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView tdItem;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            tdItem = itemView.findViewById(android.R.id.text1);
+        }
+
+        //update the view of the data inside of the view holder with this data
+        public void bind(String item) {
+            tdItem.setText(item);
+            tdItem.setOnLongClickListener(new View.OnLongClickListener() {
+                @Override
+                public boolean onLongClick(View v) {
+                    //notify the listener which position was long pressed
+                    longClickListener.onItemLongClicked(getAdapterPosition());
+                    return true;
+                }
+            });
+
+        }
+    }
 }
--- app/src/main/java/com/example/simpletodo/MainActivity.java
+++ app/src/main/java/com/example/simpletodo/MainActivity.java
@@ -1,12 +1,23 @@
 package com.example.simpletodo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ClipData;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -17,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
     Button btnAdd;
     EditText edtItem;
     RecyclerView tdItems;
+    ItemsAdapter itemsAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,8 +39,62 @@ public class MainActivity extends AppCompatActivity {
         edtItem = findViewById(R.id.edtItem);
         tdItems = findViewById(R.id.tdItems);
 
-        items = new ArrayList<>();
-        items.add("Ab");
-        items.add("abc");
+        loadItems();
+
+        ItemsAdapter.OnLongClickListener onLongClickListener = new ItemsAdapter.OnLongClickListener() {
+            @Override
+            public void onItemLongClicked(int position) {
+                //delete item from model
+                items.remove(position);
+                //notify the adapter
+                itemsAdapter.notifyItemRemoved(position);
+                Toast.makeText(getApplicationContext(), "Item has been removed.", Toast.LENGTH_SHORT).show();
+                saveItems();
+            }
+        };
+
+        itemsAdapter = new ItemsAdapter(items, onLongClickListener);
+        tdItems.setAdapter(itemsAdapter);
+        tdItems.setLayoutManager(new LinearLayoutManager(this));
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String todoItem = edtItem.getText().toString();
+                items.add(todoItem);
+                itemsAdapter.notifyItemInserted(items.size() - 1);
+                edtItem.setText("");
+                Toast.makeText(getApplicationContext(), "Item has been added.", Toast.LENGTH_SHORT).show();
+                saveItems();
+            }
+        });
     }
+
+
+    private File getDataFile() {
+        return new File(getFilesDir(), "data.txt");
+    }
+
+    //this function will load items by reading the data file
+    private void loadItems() {
+        try {
+            items = new ArrayList<>(FileUtils.readLines(getDataFile(), Charset.defaultCharset()));
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("MainActivity", "Error reading items", e);
+            items = new ArrayList<>();
+        }
+    }
+
+    //this function will write items to the data file
+    private void saveItems(){
+        try {
+            FileUtils.writeLines(getDataFile(),items);
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e("MainActivity","error writing items",e);
+        }
+    }
+
+
 }
